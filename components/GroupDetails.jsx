37394623ad1734ab57a2b0f9bc6fa271823fc90f
mr-bmv <mr.m.botkin@gmail.com>
2020-12-19 22:31:29 +0300

    GroupDetails created

---
+++ components/GroupDetails.jsx
@@ -0,0 +1,85 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react'
+import { View, Text, Button, Image, StyleSheet, ScrollView, Pressable, TouchableOpacity } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler';
+import { COLORS, width } from '../constants/colors';
+import { useRandomContext } from '../context/RandomContext';
+import dummy from '../services/dummyUsers'
+
+const GroupDetails = () => {
+    const { theme } = useRandomContext();
+    const navigation = useNavigation();
+
+    const renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity
+                style={[styles.card, { backgroundColor: theme.backgroundColorLight }]}
+                onPress={() => { navigation.navigate('Profile', { item }) }}
+            >
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Image
+                        style={styles.image}
+                        source={{
+                            uri: `${item.img.large}`,
+                        }}
+                    />
+                    <Text style={[styles.text, { color: theme.mainText, }]}>{item.name}</Text>
+                </View>
+                <Pressable
+                    style={styles.checkBox}
+                    onPress={() => console.warn("Delete group")}
+                >
+                    <MaterialIcons name="delete-outline" size={34} color={COLORS.dark.warning} />
+                </Pressable>
+            </TouchableOpacity>
+        )
+    };
+
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", backgroundColor: theme.backgroundColor }}
+        >
+            <Text style={[styles.title, { color: theme.primary }]}>Group Details</Text>
+            <Text style={[styles.qty, { color: theme.primary }]}>QTY</Text>
+            <FlatList
+                renderItem={renderItem}
+                data={dummy}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    )
+};
+
+export default GroupDetails;
+
+const styles = StyleSheet.create({
+    card: {
+        width: width - 30,
+        height: 100,
+        margin: 8,
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: 20,
+        justifyContent: "space-between"
+    },
+    image: {
+        width: 80,
+        height: 80,
+        marginLeft: 10,
+        borderRadius: 13
+    },
+    text: {
+        fontSize: 18,
+        marginLeft: 20,
+    },
+    title: {
+        fontSize: 32
+    },
+    qty: {
+        fontSize: 20
+    },
+    checkBox: {
+        marginRight: 25
+    }
+});
