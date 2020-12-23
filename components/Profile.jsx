75b49c9505dfa5fa1372163b44ec58b24d2e31f5
mr-bmv <mr.m.botkin@gmail.com>
2020-12-23 17:01:46 +0300

    Generate String of groups where user contained

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,121 +3,128 @@ import { View, Text, Button, Image, StyleSheet, ScrollView, Pressable } from 're
 import { width } from '../constants/colors';
 import { useRandomContext } from '../context/RandomContext';
 import { nationality } from "../assets/mapping"
+import getUserGroups from '../utils/getUserGroups';
 
 const getNationality = (text) => {
-    return nationality[text]
+  return nationality[text]
 };
 
 const Profile = ({ navigation, route }) => {
-    /* 2. Get the param */
-    const { name, age, phone, email, username, address, nationality, city, img, id } = route.params.item;
-    const item = route.params;
-    const { theme, setGroup } = useRandomContext();
-    // const id = dummyUser.id
-    return (
-        <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
-            {/* Name and Photo */}
-            <View style={styles.header}>
-                <Text style={[styles.name, { color: theme.success }]} >{name}</Text>
-                <Image
-                    style={styles.photo}
-                    source={{
-                        uri: `${img.large}`,
-                    }}
-                />
-            </View>
-            {/* Person Info */}
-            <ScrollView>
-                <View style={styles.info}>
-                    <View style={styles.row}>
-                        <Text style={[styles.text, { color: theme.success }]}>Age  </Text>
-                        <Text style={[styles.text, { color: theme.mainText }]}>{age}</Text>
-                    </View>
+  /* 2. Get the param */
+  const { name, age, phone, email, username, address, nationality, city, img, id } = route.params.item;
+  const item = route.params;
+  const { theme, state } = useRandomContext();
+  // const id = dummyUser.id
+  const groupsString = getUserGroups(state.groups, id);
+  console.log("----> ", groupsString)
+  return (
+    <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
+      {/* Name and Photo */}
+      <View style={styles.header}>
+        <Text style={[styles.name, { color: theme.success }]} >{name}</Text>
+        <Image
+          style={styles.photo}
+          source={{
+            uri: `${img.large}`,
+          }}
+        />
+      </View>
+      {/* Person Info */}
+      <ScrollView>
+        <View style={styles.info}>
+          <View style={styles.row}>
+            <Text style={[styles.text, { color: theme.success }]}>Age  </Text>
+            <Text style={[styles.text, { color: theme.mainText }]}>{age}</Text>
+          </View>
 
-                    <View style={styles.row}>
-                        <Text style={[styles.text, { color: theme.success }]}>Phone  </Text>
-                        <Text style={[styles.text, { color: theme.mainText }]}>{phone}</Text>
-                    </View>
+          <View style={styles.row}>
+            <Text style={[styles.text, { color: theme.success }]}>Phone  </Text>
+            <Text style={[styles.text, { color: theme.mainText }]}>{phone}</Text>
+          </View>
 
-                    <View style={styles.row}>
-                        <Text style={[styles.text, { color: theme.success }]}>E-mail  </Text>
-                        <Text style={[styles.text, { color: theme.mainText }]}>{email}</Text>
-                    </View>
+          <View style={styles.row}>
+            <Text style={[styles.text, { color: theme.success }]}>E-mail  </Text>
+            <Text style={[styles.text, { color: theme.mainText }]}>{email}</Text>
+          </View>
 
-                    <View style={styles.row}>
-                        <Text style={[styles.text, { color: theme.success }]}>Address  </Text>
-                        <Text style={[styles.text, { color: theme.mainText }]}>{city}</Text>
-                    </View>
+          <View style={styles.row}>
+            <Text style={[styles.text, { color: theme.success }]}>Address  </Text>
+            <Text style={[styles.text, { color: theme.mainText }]}>{city}</Text>
+          </View>
 
-                    <View style={styles.row}>
-                        <Text style={[styles.text, { color: theme.success }]}>Nationality  </Text>
-                        <Text style={[styles.text, { color: theme.mainText }]}>{getNationality(nationality)}</Text>
-                    </View>
+          <View style={styles.row}>
+            <Text style={[styles.text, { color: theme.success }]}>Nationality  </Text>
+            <Text style={[styles.text, { color: theme.mainText }]}>{getNationality(nationality)}</Text>
+          </View>
 
-                    <View style={styles.row}>
-                        <Text style={[styles.text, { color: theme.success }]}>Groups  </Text>
-                        <Text style={[styles.text, { color: theme.warning }]}> Need to add </Text>
-                    </View>
+          <View style={styles.row}>
+            <Text style={[styles.text, { color: theme.success }]}>Groups  </Text>
+            <Text style={[styles.text, { color: theme.warning, width: 220 }]}
+              numberOfLines={1}
+            > {groupsString} </Text>
+          </View>
 
 
-                    {/* get params from SecondScreen */}
-                    {/* {!route.params?.post || <Text>Note: {route.params?.post}</Text>} */}
+          {/* get params from SecondScreen */}
+          {/* {!route.params?.post || <Text>Note: {route.params?.post}</Text>} */}
 
-                </View >
-            </ScrollView >
-            {/*
+        </View >
+      </ScrollView >
+      {/*
             <Button title="Go MainList" onPress={() => navigation.navigate('Main List')} />
             <Button title="Go to Second" onPress={() => navigation.navigate('SecondScreen')} />
             <Button title="Go back" onPress={() => navigation.goBack()} /> */}
-            <Pressable
-                style={[styles.button, { backgroundColor: theme.primary, }]}
-                // onPress={() => setGroup({ [id]: route.params.item })}
-                onPress={() => navigation.navigate("Add User", { item })}
-            >
+      <Pressable
+        style={[styles.button, { backgroundColor: theme.primary, }]}
+        // onPress={() => setGroup({ [id]: route.params.item })}
+        onPress={() => navigation.navigate("Add User", { item })}
+      >
 
-                <Text style={styles.buttonText}>Add Group</Text>
-            </Pressable>
-        </View >
-    )
+        <Text style={styles.buttonText}>Add Group</Text>
+      </Pressable>
+    </View >
+  )
 }
 
 export default Profile;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        alignItems: "center"
-    },
-    name: {
-        fontWeight: "700",
-        fontSize: 30,
-        marginVertical: 5
-    },
-    photo: {
-        width: width - 50,
-        aspectRatio: 1 / 1,
-    },
-    info: {
-        paddingLeft: 25,
-        marginTop: 10
-    },
-    text: {
-        fontSize: 18,
-        lineHeight: 28,
-    },
-    row: { flexDirection: "row" },
-    button: {
-        marginHorizontal: 25,
-        borderRadius: 10,
-        marginBottom: 15,
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 46
-    },
-    buttonText: {
-        fontSize: 26,
-        color: "#fff"
-    },
+  container: {
+    flex: 1,
+  },
+  header: {
+    alignItems: "center"
+  },
+  name: {
+    fontWeight: "700",
+    fontSize: 30,
+    marginVertical: 5
+  },
+  photo: {
+    width: width - 50,
+    aspectRatio: 1 / 1,
+  },
+  info: {
+    paddingLeft: 25,
+    marginTop: 10
+  },
+  text: {
+    fontSize: 18,
+    lineHeight: 28,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  button: {
+    marginHorizontal: 25,
+    borderRadius: 10,
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 46
+  },
+  buttonText: {
+    fontSize: 26,
+    color: "#fff"
+  },
 });
---
+++ utils/getUserGroups.js
@@ -0,0 +1,28 @@
+/**
+ * Generate string which contain all groups where user contained
+ * @param {object} groups
+ * @param {object} user
+ * return string
+ */
+const getUserGroups = (groups, user) => {
+    console.log('Start generate string')
+    console.log(' Object of groups')
+    console.log(groups)
+    console.log('----------------')
+    console.log('User')
+    console.log(user)
+    return Object.keys(groups).reduce((prevValue, group) => {
+        if (groups[group].indexOf(user) > -1) {
+            const groupStr = String(group);
+            const result = `${prevValue} ${groupStr} /`
+            console.log("RESULT")
+            console.log(result)
+            return result
+        }
+        console.log('No')
+        return prevValue;
+    },
+        '')
+}
+
+export default getUserGroups;
